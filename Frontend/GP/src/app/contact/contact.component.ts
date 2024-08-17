042e01723a30a1d1eee276989b3b993aac19915d
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-17 13:20:30 +0300

    Adding contact ui

--- Frontend/GP/src/app/contact/contact.component.ts
+++ Frontend/GP/src/app/contact/contact.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public contactInfo = {
+    title: "Contact Me ☎️",
+    subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
+    number: "+91-9634018431",
+    email_address: "sharthak31@gmail.com"
+  };
+  socialMediaLinks = {
+
+    github: "https://github.com/sarthakgoenka",
+    linkedin: "https://www.linkedin.com/in/sarthak-agrawal-a41aa3153/",
+    gmail: "sharthak31@gmail.com",
+    instagram : "https://www.instagram.com/sarthak_goenka/?hl=en",
+    facebook: "https://www.facebook.com/sharthak.agrawal"
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
