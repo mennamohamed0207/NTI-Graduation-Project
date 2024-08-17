ef01cf750341c1ff072654a083f87f66be24769e
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-17 14:02:46 +0300

    Contacts reconstructed

--- Frontend/GP/src/app/contact/contact.component.ts
+++ Frontend/GP/src/app/contact/contact.component.ts
@@ -1,65 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ExperienceService } from '../services/experience.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrl: './contact.component.css'
 })
-export class ContactComponent implements OnInit {
-  public number: string = '';
-  public emailAddress: string = '';
-  public github: string = '';
-  public linkedin: string = '';
-  public facebook: string = '';
+export class ContactComponent {
+  
 
-  public contactInfo = {
-    title: "Contact Me ☎️",
-    subtitle: "Get in touch with me",
-    number: '',
-    email_address: '',
-  };
-  socialMediaLinks = {
-
-    github: "https://github.com/sarthakgoenka",
-    linkedin: "https://www.linkedin.com/in/sarthak-agrawal-a41aa3153/",
-    gmail: "sharthak31@gmail.com",
-    instagram : "https://www.instagram.com/sarthak_goenka/?hl=en",
-    facebook: "https://www.facebook.com/sharthak.agrawal"
-  };
-  constructor(private dataService: ExperienceService) {}
-
-  ngOnInit(): void {
-    this.dataService.getContacts().subscribe((data) => {
-      // this.mapContactLinks(data.data);
-      console.log(data.data);
-      
-    })
-  }
-
-  // private mapContactLinks(data: any): void {
-  //   data.forEach((item: any) => {
-  //     switch (item.name.toLowerCase()) {
-  //       case 'phone':
-  //         this.number = item.link;
-  //         this.contactInfo.number = this.number;
-  //         break;
-  //       case 'gmail':
-  //         this.emailAddress = item.link;
-  //         this.contactInfo.email_address = this.emailAddress;
-  //         break;
-  //       case 'github':
-  //         this.github = item.link;
-  //         break;
-  //       case 'linkedin':
-  //         this.linkedin = item.link;
-  //         break;
-  //       case 'facebook':
-  //         this.facebook = item.link;
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   });
-  // }
 }
