33f190614ad8cc25ad3bede6e44c595d26c18021
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-18 03:07:22 +0300

    Adding education in front

--- Frontend/GP/src/app/education/education.component.ts
+++ Frontend/GP/src/app/education/education.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
-
+  public college="Faculy of Engineering";
+  public degree="Bachelor Degree";
+  public fromDate="Sep.2020";
+  public toDate="Jun.2025";
+  public university="Cairo University";
+  public department ="Computer Engineering Department"
+  public totalPercent="88%"
 }
