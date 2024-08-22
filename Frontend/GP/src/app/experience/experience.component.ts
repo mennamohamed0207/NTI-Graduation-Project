7367feeeb9a7cd060b4c60685cf0d609450d0641
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-22 16:00:36 +0300

    adding experience but without trainings  without backend

--- Frontend/GP/src/app/experience/experience.component.ts
+++ Frontend/GP/src/app/experience/experience.component.ts
@@ -8,56 +8,68 @@ import { Subscription } from 'rxjs';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
-export class ExperienceComponent implements OnInit,OnDestroy {
+export class ExperienceComponent implements OnInit, OnDestroy {
 
-  timeline: any;
+  timeline = [
+    {
+      "title": "IT Intern",
+      "org": "Seoudi Supermarket",
+      "fromDate": "Aug.2023",
+      "toDate": "Sep.2023",
+      "description": ["Assisted with network setup and system administration tasks within the infrastructure team.",
+        "Attended sessions on web development workflow and IT helpdesk ticket system, enhancing understanding of IT operations."],
+      "tools": ["Networks", "SQL", "ERP"]
+    },
+    {
+      "title": "Backend Intern",
+      "org": "Cargas",
+      "fromDate": "Jul.2023",
+      "toDate": "Aug.2023",
+      "description": ["Contributed to the HR system website, enhancing employee experience with hotel reservations, unit resorts, coupons, and financial requests.",
+        "Implemented user management, advanced payment for schools, and resort modules."],
+      "githubLink": "https://github.com/mennamohamed0207/CarGas",
+      "tools": ["Laravel", "MySQL"]
+    },
+    {
+      "title": "Functional Verification Diploma Simulation Track Training",
+      "org": "Siemens",
+      "fromDate": "Feb.2023",
+      "toDate": "Apr.2023",
+      "description": [""],
+      "tools": ["", "MySQL"]
+    },
+
+    {
+      "title": "Trainee",
+      "org": "Orange Digital Center",
+      "fromDate": "Jul.2022",
+      "toDate": "Oct.2022",
+      "description": ["Built an AgriTech service for soil element monitoring including a protards, improving agricultural efficiency",
+        "Secured first place in the Orange Summer Challenge 2022, demonstrating exceptional innovation and technical prowess."],
+      "githubLink": "https://github.com/mennamohamed0207/InSoil",
+      "tools": ["Flutter", " Arduino", "Fab Lab devices"]
+    },
+
+
+  ];
   private subscription: Subscription | undefined;
 
-  constructor(private dataService: ExperienceService, private route: ActivatedRoute) {
-    console.log("experience Con");
-    // this.router.events.subscribe((event) => {
-    //   if (event instanceof NavigationEnd) {
-    //     this.loadData(); // Trigger data loading on navigation
-    //   }
-    // });
-  //  this.loadData();
-  }
-  // ngOnChanges(): void {
-  //   console.log("experience change");
+  constructor() {
 
-  //   this.loadData();  // Load data whenever inputs change
-  // }
-
-  loadData(): void {
-    this.subscription = this.dataService.getExperience().subscribe(
-      (data) => {
-        this.timeline = data.data;
-      }
-    );
-  }
- 
-  
-  ngAfterViewInit(): void {
-    console.log("experience view init");
-    
-    this.loadData();  // Called after the component's view has been initialized
   }
 
-  // ngOnInit(): void {
-  //   console.log("experience init ");
-  //   this.loadData();
-  
-  // }
+
+
+
+
+
+
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.loadData();
-    });
+
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+
   }
 }
