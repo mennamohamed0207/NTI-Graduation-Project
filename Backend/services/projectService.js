9ba748e628e804347ab863ef17cadf15f7cbc2bf
mennamohamed0207 <menamohamed0207@gmail.com>
2024-08-29 17:14:58 +0300

    Modifying in projects

--- Backend/services/projectService.js
+++ Backend/services/projectService.js
@@ -3,8 +3,8 @@ const projectModel=require('../models/projectsModel');
 
 exports.createProject=async(req,res)=> {
     try {
-      req.body.tools=req.body.tools.split(",");
-      req.body.languages=req.body.languages.split(",");
+      req.body.tools=req.body.tools.split("|");
+      req.body.languages=req.body.languages.split("|");
       const data = await projectModel.create(req.body);
       res.status(200).json({ message: "Project added successfully", data: data });
     } catch (error) {
